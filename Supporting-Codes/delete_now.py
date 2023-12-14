98575f281df64b0f87518804c8f31909880211ad
Amit Kumar <amitkm5364@gmail.com>
2023-12-14 22:17:47 +0530

    Token is successfully working

--- Supporting-Codes/delete_now.py
+++ Supporting-Codes/delete_now.py
@@ -35,18 +35,19 @@ import jwt
 from datetime import datetime, timedelta
 import time
 
-SECRET_KEY = 'your_secret_key'
+SECRET_KEY = 'your_secret_keynxcwionfoeuiwouh@&#*(@&!_jnjind_----jcdnijw-00329jdnuenf3894njekdncf9384nfjndfc9384fndjcv98o34inf9ec8j438idm4398j)'
+SECRET_KEY1 = 'your_secret_keynxcwionfoeuiwouh@&#*(@&!_jnjind_----jcdnijw-00329jdnuenf3894njekdncf9384nfjndfc9384fndjcv98o34inf9ec8j438idm4398j)'
 
 def encode_token(payload):
     # Encode the JWT token
-    return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return jwt.encode(payload, SECRET_KEY, algorithm='HS512')
 
 def decode_and_verify_token(token):
     try:
         # If the token is a string, convert it to bytes before decoding
         if isinstance(token, str):
             token = token.encode('utf-8')
-        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        decoded_token = jwt.decode(token, SECRET_KEY1, algorithms=['HS512'])
         print("Decoded Token:", decoded_token)
         return decoded_token
     except jwt.ExpiredSignatureError as e:
@@ -68,13 +69,18 @@ def is_token_expired(token):
 payload = {
     'user_id': 123,
     'username': 'example_user',
-    'exp': datetime.utcnow() + timedelta(seconds=10)
+    'exp': datetime.utcnow() + timedelta(seconds=11),
+    'iat': datetime.utcnow(),
+    'nbf': datetime.utcnow(),
+    'jti': '1234567890',
+    'role': 'admin',
+    'iss': 'example_issuer'
 }
 
 encoded_token = encode_token(payload)
 print("Encoded Token:", encoded_token)
 
-time.sleep(9)
+time.sleep(1)
 
 # Check if the token has expired
 if is_token_expired(encoded_token):
--- handlers/__init__.py
+++ handlers/__init__.py
@@ -46,57 +46,96 @@ from pytz import timezone
 
 from .FloorIncharge import FloorIncharge1
 
-def create_tocken(username):
-    app = create_app()  # Assuming create_app() is defined somewhere
+# def create_tocken(username):
+#     app = create_app()  # Assuming create_app() is defined somewhere
 
-    # Get the current time in UTC
-    current_utc_time = datetime.utcnow()
+#     # Get the current time in UTC
+#     current_utc_time = datetime.utcnow()
     
-    # Define the IST timezone
-    ist_timezone = timezone('Asia/Kolkata')  # IST is UTC+5:30
+#     # Define the IST timezone
+#     ist_timezone = timezone('Asia/Kolkata')  # IST is UTC+5:30
 
-    # Convert the current UTC time to IST
-    current_ist_time = current_utc_time.astimezone(ist_timezone)
+#     # Convert the current UTC time to IST
+#     current_ist_time = current_utc_time.astimezone(ist_timezone)
 
-    # Set the expiration time to 120 seconds in the future in IST
-    expiration_time_ist = current_ist_time + timedelta(seconds=120)
+#     # Set the expiration time to 120 seconds in the future in IST
+#     expiration_time_ist = current_ist_time + timedelta(seconds=120)
     
-    # Print the expiration time for debugging
-    print("Token Expiry Time:", expiration_time_ist)
+#     # Print the expiration time for debugging
+#     print("Token Expiry Time:", expiration_time_ist)
 
+#     # Encode the JWT token
+#     token = jwt.encode({
+#         'user': username,
+#         'exp': expiration_time_ist,  # 'exp' is the standard claim for expiration time
+#     }, app.config['SECRET_KEY'], algorithm='HS256')
+    
+#     # print(type(token))
+#     return jsonify({'token': token})
+
+# # Example of decoding and verifying the token
+# def decode_and_verify_token(token):
+#     app = create_app()
+#     try:
+#         # If the token is a string, convert it to bytes before decoding
+#         print(token)
+#         if isinstance(token, str):
+#             token = token.encode('utf-8')
+#             print(token)
+#         decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+#         print(type(token))
+#         print("Decoded Token:", decoded_token)
+#         return decoded_token
+#     except jwt.ExpiredSignatureError as e:
+#         print("Token has expired", e)
+#         return None
+
+# # Example of checking token expiration
+# def is_token_expired(token):
+#     decoded_token = decode_and_verify_token(token)
+#     if decoded_token:
+#         expiration_time = datetime.utcfromtimestamp(decoded_token['exp'])
+#         print(expiration_time)
+#         current_utc_time = datetime.utcnow()
+#         print(current_utc_time)
+#         return expiration_time <= current_utc_time
+#     return True
+
+
+
+
+
+
+def create_tocken(username, user_id):
+    payload = {
+        'user_id': user_id,
+        'username': username,
+        'exp': datetime.utcnow() + timedelta(seconds=110),
+    }
+    app = create_app()
     # Encode the JWT token
-    token = jwt.encode({
-        'user': username,
-        'exp': expiration_time_ist,  # 'exp' is the standard claim for expiration time
-    }, app.config['SECRET_KEY'], algorithm='HS256')
-    
-    # print(type(token))
-    return jsonify({'token': token})
+    return jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS512')
 
-# Example of decoding and verifying the token
 def decode_and_verify_token(token):
     app = create_app()
     try:
         # If the token is a string, convert it to bytes before decoding
-        print(token)
         if isinstance(token, str):
             token = token.encode('utf-8')
-            print(token)
-        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        print(type(token))
+        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS512'])
         print("Decoded Token:", decoded_token)
         return decoded_token
     except jwt.ExpiredSignatureError as e:
-        print("Token has expired", e)
+        print("Token has expired:", e)
+        return None
+    except jwt.DecodeError as e:
+        print("Error decoding token:", e)
         return None
 
-# Example of checking token expiration
 def is_token_expired(token):
     decoded_token = decode_and_verify_token(token)
     if decoded_token:
         expiration_time = datetime.utcfromtimestamp(decoded_token['exp'])
-        print(expiration_time)
         current_utc_time = datetime.utcnow()
-        print(current_utc_time)
         return expiration_time <= current_utc_time
     return True
